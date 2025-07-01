fd2747fb32eb261e2c6c1d80a624c604a53133ae
senstella <senstella01@gmail.com>
2025-07-01 15:38:08 +0900

    added parakeet varients to __init__ of package

--- parakeet_mlx/__init__.py
+++ parakeet_mlx/__init__.py
@@ -1,11 +1,31 @@
 from parakeet_mlx.alignment import AlignedResult, AlignedSentence, AlignedToken
-from parakeet_mlx.parakeet import DecodingConfig, ParakeetTDT, ParakeetTDTArgs
+from parakeet_mlx.parakeet import (
+    BaseParakeet,
+    DecodingConfig,
+    ParakeetCTC,
+    ParakeetCTCArgs,
+    ParakeetRNNT,
+    ParakeetRNNTArgs,
+    ParakeetTDT,
+    ParakeetTDTArgs,
+    ParakeetTDTCTC,
+    ParakeetTDTCTCArgs,
+    StreamingParakeet,
+)
 from parakeet_mlx.utils import from_pretrained
 
 __all__ = [
     "DecodingConfig",
     "ParakeetTDTArgs",
     "ParakeetTDT",
+    "ParakeetRNNT",
+    "ParakeetRNNTArgs",
+    "ParakeetCTC",
+    "ParakeetCTCArgs",
+    "ParakeetTDTCTC",
+    "ParakeetTDTCTCArgs",
+    "BaseParakeet",
+    "StreamingParakeet",
     "from_pretrained",
     "AlignedResult",
     "AlignedSentence",
